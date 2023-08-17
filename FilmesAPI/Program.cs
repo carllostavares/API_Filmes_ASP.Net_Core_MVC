f6bc35c641505b7d2e36980111a762de1e5957ec
Carlos Tavares <carlostavares2350@gmail.com>
2023-08-17 11:47:19 -0300

    Adicionando a autenticação com o Banco com o connectionString no Program.

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -4,6 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
+
+builder.Services.AddDbContext<FIlmeContext>(opts => 
+opts.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
