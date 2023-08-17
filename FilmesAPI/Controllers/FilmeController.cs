17adfcf49f222d0b758b167041b2d474fca7dfc7
Carlos Tavares <carlostavares2350@gmail.com>
2023-08-17 10:19:07 -0300

    Adicionando projeto Base que estava sendo implementado no reprositório de Atividades Pratica de C#. ONde será implementado para fins de estudo neste nosso repositótio.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class FilmeController : Controller
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody]Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperaFilmePorID),
+            new {id = filme.Id},
+            filme );
+ 
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0,
+        [FromQuery] int take =10)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorID(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok();
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="É obrigatório informar o  Título !")]
+    [MaxLength(50, ErrorMessage = "O tamanho do Título não pode exceder 50 caracteres !")]
+
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar o Genero !")]
+    [MaxLength(50,ErrorMessage ="O tamanho do Genero não pode exceder 50 caracteres !")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar a Duração !")]
+    [Range(70,600, ErrorMessage = "O tamanho do Genero não pode exceder 50 caracteres !")]
+    public int Duracao { get; set; }
+
+}
